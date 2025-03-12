d1fe3cafead3d9d32e2009addac6f532bf85f585
Francisco Antonio <168096368+FranciscoAntonioSilva@users.noreply.github.com>
2025-03-12 12:26:17 -0300

    Aprendendo condições simples

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70.1
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) { //Condição Simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log(`Diriga sempre com cuidado!`)
