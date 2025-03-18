812f85d3758d1713518fc13525fc1e50c70766a6
Francisco Antonio <168096368+FranciscoAntonioSilva@users.noreply.github.com>
2025-03-18 09:48:20 -0300

    fazendo exercícios de javascript

--- aula12ex/modelo/script.js
+++ aula12ex/modelo/script.js
@@ -7,9 +7,12 @@ function carregar() {
 
     if (hora >= 0 && hora < 12) {
         img.src = 'images/fotomanha.jpg'
+        document.body.style.background = '#e2cd9f'
     } else if (hora >= 12 && hora < 18) {
         img.src = 'images/fototarde.jpg'
+        document.body.style.background = '#C95F12'
     } else {
         img.src = 'images/fotonoite.jpg'
+        document.body.style.background = '#06316E'
     }
 }
