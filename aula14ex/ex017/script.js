5c82e7859e30e21ae81967a76f0f34a1046e29c6
Francisco Antonio <168096368+FranciscoAntonioSilva@users.noreply.github.com>
2025-04-04 12:41:58 -0300

    Fazendo novos exercícios

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,18 +1,20 @@
-function carregar() {
-    var msg = document.getElementById('msg')
-    var img = document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
+function tabuada() {
+    let num = document.getElementById('num').value
+    let tabuada = document.getElementById('tab')
 
-    if (hora >= 0 && hora < 12) {
-        img.src = 'images/fotomanha.jpg'
-        document.body.style.background = '#e2cd9f'
-    } else if (hora >= 12 && hora < 18) {
-        img.src = 'images/fototarde.jpg'
-        document.body.style.background = '#C95F12'
+    if (num.length == 0) {
+        alert('Por favor, digite um número!')
     } else {
-        img.src = 'images/fotonoite.jpg'
-        document.body.style.background = '#06316E'
+        let n = Number(num)
+        let c = 1
+        tabuada.innerHTML = ''
+
+        while(c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} * ${c} = ${num*c}`
+            item.value = `tab${c}`
+            tabuada.appendChild(item)
+            c++
+        }
     }
 }
